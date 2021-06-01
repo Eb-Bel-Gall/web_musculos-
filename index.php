847b7d93a67fb5ba2772275dfb25c60a2f84a8de
E.Bel.Gall <85178848+Eb-Bel-Gall@users.noreply.github.com>
2021-06-01 10:49:59 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html PUBLIC >
+<html lang="es"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1"><meta name="description" content=""><meta name="author" content="">
+<head>
+<title>Musculos | PAGINA DE INICIO</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="css/coin-slider.css" />
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/cufon-aller.js"></script>
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<script type="text/javascript" src="js/coin-slider.min.js"></script>
+</head>
+<body>
+<div class="main">
+  <div class="header">
+    <div class="header_resize">
+      <div class="logo">
+       <h1><a href="index.html">MUSCULOS </a> </h1>
+      </div>
+      <div class="menu_nav">
+        <ul>
+          
+          <li><a href="index_login.php"><span></span>Consultar</span></a></li>
+        </ul>
+      </div>
+      <div class="clr"></div>
+      <div class="slider">
+        <div id="coin-slider"> <a href="#"><img src="images/musculos2.png" width="960" height="320" alt="" /> </a> <a href="#"><img src="images/p2.jpg" width="960" height="320" alt="" /> </a> <a href="#"><img src="images/p5.png" width="960" height="960" alt="" /> </a> </div>
+        <div class="clr"></div>
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>   
+      
+  <div class="fbg">
+    <div class="fbg_resize">
+      <div class="col c1">
+        
+      <div class="col c2">
+        <h2><span>Servicios</span> </h2>
+
+        <table>
+          <tr><td align="justify"> <p> La web Musculos,te ayuda a capacitarte e informarte,acerca de los diferentes tipos de musculos del cuerpo humano,
+            tales como: estructura,forma,clasificacion y traducciones en diferentes idiomas.</p></td></tr>
+        </table> 
+        
+        <ul class="fbg_ul">
+          <li><a href="visitante/estruc.php">Estructura.</a></li>
+          <li><a href="visitante/forma.php">Formas.</a></li>
+          <li><a href="visitante/clas.php">Clasificacion.</a></li>
+          <li><a href="visitante/tradu.php">Tipo de Musculos y su Traduccion.</a></li>
+          <li><a href="visitante/idiom.php">Idiomas.</a></li>
+        </ul>
+          <div class="col c3">
+            <font face='verdana'>
+              <h3> Muscle-Web</h3>
+            </font>
+          </div>
+    <div class="footer-col col-md-4">
+            
+             <img src="images/unicordoba.png" width="250" height="250" class="img-responsive img-centered" alt="">
+             <font face='verdana'> Programacion web </font> </strong>
+             <h3>Location</h3>
+              <font face='verdana'>Facultad de Educacion y Ciencias Humanas </font> </strong> 
+               <font face='verdana'>Licenciatura en Informatica y Medios Audiovisuales </font> </strong> 
+                <br>
+     <div class="clr"></div>
+      </div>
+      
+      <div class="col c2">
+           <h2><span>Contacto  </span> Colombia</h2>
+               <p>Puedes contactarnos :
+              Ebby Beltran Gallego, Jaime Vitar Olascuaga, Johan Cuervo Osorio</p>
+             <p class="contact_info"> <span>Colombia:</span> Monteria<br />
+          <span>Telefono:</span> 123456789<br />
+          <span>FAX:</span> +57-4578<br />
+          <span>Direccion:</span> mzn 126 lt 8 B/floresta<br />
+          <span>E-mail:</span> basededatos@hotmail.com  </p>
+
+      </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  <div class="col c3"> 
+           <font face='verdana'><h3>  Muscle-Web  &copy; 2017</h3></font> 
+               </div>     
+      <div style="clear:both;"></div>
+    </div>
+  </div>
+</div>
+<a href="#"><img src="images/arriba.png" width="75" height="75" title="Ir arriba" style="position: fixed; bottom: 1px; left: 92%;" /></a>
+</html>
